d885b7b030b70c6b5fa4a6f509110068f102d16c
Jakub Ner <jakub_ner@hotmail.com>
2021-10-29 16:20:34 -0700

    rename script

--- rename.ps1
+++ rename.ps1
@@ -1,7 +1,7 @@
 $prefix="vanilla_"
 Get-ChildItem -path "." -file -force -recurse -filter "*.xml" | foreach-object {
-	if (!$_.name.contains("joat")) {
-		cd  $_.DirectoryName
-		mv "$_" "${prefix}_$($_.BaseName).xml"
+	if (!$_.name.contains("joat") -and !$_.name.contains("vanilla_") -and ! $_.PSIsContainer) {
+		Write-Host cd  $_.DirectoryName
+		#mv "$_" "${prefix}_$($_.BaseName).xml"
 	}
 }
