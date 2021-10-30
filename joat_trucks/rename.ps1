087bce2bd23840c4699d9a63ff06765566f78e1e
Jakub Ner <jakub_ner@hotmail.com>
2021-10-29 17:23:10 -0700

    rename script

--- joat_trucks/rename.ps1
+++ joat_trucks/rename.ps1
@@ -1,31 +1,39 @@
 # Script to rename addons such that they don't appear twice in the mod.
 
-$prefix="vanilla_"
+$prefix="vanilla__"
 $pwd = pwd
 Get-ChildItem -path "./classes" -file -force -recurse -filter "*.xml" | foreach-object {
 	if (!$_.name.contains("joat") -and !$_.name.contains($prefix) -and ! $_.PSIsContainer) {
 		cd  $_.DirectoryName
-		mv "$_" "${prefix}_$($_.BaseName).xml"		
+		mv "$_" "${prefix}$($_.BaseName).xml"		
 	}
 }
 
-$names = $()
+$names = @()
 
 Get-ChildItem -path "./classes" -file -force -recurse -filter "*.xml" | foreach-object {
 	if (!$_.name.contains("joat") -and ! $_.PSIsContainer) {
-		$names += $_.BaseName.replace("vanilla_", "")
+		$names += $_.BaseName.replace($prefix, "")
 	}
 }
 
 cd $pwd/classes/trucks
 
-$names | ForEach {
-	$nameToChange = $_
-	Get-ChildItem -path "." -file -filter "*.xml" | foreach-object {
-		if ($_.name.contains("joat") -and ! $_.PSIsContainer) {
-			((Get-Content -path $_ -Raw) -replace "$nameToChange","vanilla_$nameToChange") | Set-Content -Path $_
-		}
+Get-ChildItem -path "." -file -filter "*.xml" | foreach-object {
+	$file = $_
+	$content = Get-Content -path $file -Raw
+	Write-Host $content
+	exit
+	Write-Host "replacing for $file"
+	if ($file.name.contains("joat") -and ! $_.PSIsContainer) {
+		$names | % {
+			$nameToChange = $_
+			Write-Host "   processing $nameToChange"
+			$content.replace("$name0ToChange","vanilla_$nameToChange")
+		}				
 	}
+	
+	$content | Set-Content -Path $file
 }
 
 cd $pwd
